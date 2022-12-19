a7de46a89fe72a40778f5db8aef2574a4f87e0f9
mayjann <78281752+mayjann@users.noreply.github.com>
2022-12-19 17:42:01 +0700

    Last version update
    19.12.22

--- handlers/text_handler/text_handler.py
+++ handlers/text_handler/text_handler.py
@@ -34,21 +34,21 @@ async def menu(message: types.Message):
 # Регистрируем команду /start
 @dp.message_handler(commands="start")
 async def manual(message: types.Message):
-    
+
     # Высылаем краткий брифинг о функционале и высылаем клавиатуру.
     await message.answer(f"""
 Привет, я твой помощник для расчета времени, для того, чтобы начать, введи имя клиента, к которому приехал.
-Еще я умею вручную считать время, для этого нажми на кнопку "Ручной счет" и следуй инструкциям.    
+Еще я умею вручную считать время, для этого нажми на кнопку "Ручной счет" и следуй инструкциям.
 """, reply_markup=static_keyboards.default_keyboard)
 
 # Регистрируем нажатие на кнопку клавиатуры
 @dp.message_handler(text="Ручной счет")
 async def manual(message: types.Message, state: FSMContext):
     chatid = message.from_user.id
-    
+
     # Запускаем работу машины состояний
     await Begin.manual_time_first.set()
-    
+
     # Высылаем описание и инструкцию использования функции.
     await bot.send_message(
         chat_id=chatid,
@@ -61,22 +61,22 @@ async def manual(message: types.Message, state: FSMContext):
 async def manual_first(message: types.Message, state: FSMContext):
     chatid = message.from_user.id
     manual_first_time_raw = message.text
-    
+
     # Выполняем проверку входящего сообщения на соответствие формату.
     try:
         # Передаем переменную в функцию проверки и получаем результат.
         manual_first_time = await scripts.format_time.format_time(manual_first_time_raw)
-        
+
         # Записываем результат в память
         async with state.proxy() as data:
             data['manual_time_first'] = manual_first_time
-        
+
         # Продолжаем работу машины состояний и высылаем уведомление.
         await Begin.next()
         await bot.send_message(
             chat_id=chatid,
             text = "Отлично, теперь напиши мне время, в которое ты закончил работу.")
-    
+
     # Высылаем уведомление в случае несоответствия входящего сообщения формату.
     except:
         await bot.send_message(
@@ -87,19 +87,19 @@ async def manual_first(message: types.Message, state: FSMContext):
 # Регистрируем второй штамп времени.
 @dp.message_handler(state=Begin.manual_time_second)
 async def process_age_invalid(message: types.Message, state: FSMContext):
-    
+
     # Вытаскиваем первый штамп времени и памяти.
     async with state.proxy() as data:
         manual_first_time = data['manual_time_first']
     chatid = message.from_user.id
     manual_second_time_raw = message.text
-    
+
     # Выполняем проверку входящего сообщения на соответствие формату.
     try:
-        
+
         # Передаем переменную в функцию проверки и получаем результат.
         manual_second_time = await scripts.format_time.format_time(manual_second_time_raw)
-        
+
         # Выполняем калькуляцию затраченного времени.
         raw_minutes = await scripts.diff.minutes_make(manual_first_time, manual_second_time)
         minutes_round = await scripts.diff.minutes_round_make()
@@ -111,7 +111,7 @@ async def process_age_invalid(message: types.Message, state: FSMContext):
 Получилось {raw_minutes} минут.
 С округлением {minutes_round} минут.""")
         await state.finish()
-    
+
     # Высылаем уведомление в случае несоответствия входящего сообщения формату.
     except:
         await bot.send_message(
@@ -141,7 +141,7 @@ async def process_name(message: types.Message, state: FSMContext):
 
     # Вносим необходимую информацию в базу данных.
     await scripts.mysql.input(full_name, userid, late_dst, time_begin_raw, datejob)
-    
+
     # Уведомляем полевого инженера и центр времени.
     await message.answer(f"Информация передана первой линии.", reply_markup=inline_keyboards.end_kbd)
     await bot.send_message(
@@ -149,9 +149,9 @@ async def process_name(message: types.Message, state: FSMContext):
         text=f"""
 *Инженер* {mention} *прибыл в {late_dst} в {time_begin_raw} с опозданием.*
 Причина опоздания: {late_reason}
-""", 
+""",
         parse_mode="Markdown")
-    
+
     # Завершаем работу машины состояний.
     await state.finish()
 
@@ -164,29 +164,28 @@ async def job_begin(message: types.Message, state: FSMContext):
     userid = message.from_user.id
     user_dst = message.text
     time_begin_raw = dt.now().strftime("%H:%M")
-    
-    # Делаем запрос в базу данных на поиск выполненных визитов полевого инженера на текущий день. 
-    datejob = dt.now().strftime("%Y-%m-%d")
-    data = await scripts.mysql.output_begin(userid, datejob)
-    
-    mention = "[" + full_name + "](tg://user?id=" + str(userid) + ")"
 
-    # Выполняем проверку опоздания полевого инженера.
-    if data == 0 and time_begin_raw >= str("09:15"):
-        
-        # Запускаем машину состояний, если опоздание зарегистрировано,
-        # вносим название объекта в память и предлагаем написать причину опоздания.
-        await Begin.late.set()
-        async with state.proxy() as data:
-            data['user_late_dst'] = user_dst
-        await message.answer("Вижу, что ты опоздал к первому клиенту, напиши почему так вышло.")
-    else:
+#    # Делаем запрос в базу данных на поиск выполненных визитов полевого инженера на текущий день.
+    datejob = dt.now().strftime("%Y-%m-%d")
+#    data = await scripts.mysql.output_begin(userid, datejob)
+#
+    mention = "[" + full_name + "](tg://user?id=" + str(userid) + ")"
+#
+#    # Выполняем проверку опоздания полевого инженера.
+#    if data == 0 and time_begin_raw >= str("09:30"):
+#
+#        # Запускаем машину состояний, если опоздание зарегистрировано,
+#        # вносим название объекта в память и предлагаем написать причину опоздания.
+#        await Begin.late.set()
+#        async with state.proxy() as data:
+#            data['user_late_dst'] = user_dst
+#        await message.answer("Вижу, что ты опоздал к первому клиенту, напиши, почему так вышло.")
+#    else:
 
         # Регистрируем прибытие полевого инженера на объект, если опоздание не зарегистрировано
-        await scripts.mysql.input(full_name, userid, user_dst, time_begin_raw, datejob)
-        await message.answer(f"Информация передана первой линии.", reply_markup=inline_keyboards.end_kbd)
-        await bot.send_message(
-            chat_id=channel_id, 
-            text=f"*Инженер* {mention} *прибыл в {user_dst} в {time_begin_raw}.*",
-            parse_mode="Markdown")
-        
+    await scripts.mysql.input(full_name, userid, user_dst, time_begin_raw, datejob)
+    await message.answer(f"Информация передана первой линии.", reply_markup=inline_keyboards.end_kbd)
+    await bot.send_message(
+        chat_id=channel_id,
+        text=f"*Инженер* {mention} *прибыл в {user_dst} в {time_begin_raw}.*",
+        parse_mode="Markdown")
