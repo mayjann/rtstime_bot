51dc5d09061e3c712bcc145ba206bddf15c5a333
mayjann <78281752+mayjann@users.noreply.github.com>
2022-12-19 17:43:20 +0700

    Last version update
    19.12.2022

--- scripts/format_time.py
+++ scripts/format_time.py
@@ -2,5 +2,10 @@ import datetime
 
 async def format_time(manual_first_time_raw):
     time_obj_raw = datetime.datetime.strptime(manual_first_time_raw, '%H:%M')
-    time_obj = time_obj_raw.strftime('%H:%M')
-    return time_obj
+    manual_first_time = time_obj_raw.strftime('%H:%M')
+    return manual_first_time
+
+async def format_time(manual_second_time_raw):
+    time_obj_raw = datetime.datetime.strptime(manual_second_time_raw, '%H:%M')
+    manual_second_time = time_obj_raw.strftime('%H:%M')
+    return manual_second_time
